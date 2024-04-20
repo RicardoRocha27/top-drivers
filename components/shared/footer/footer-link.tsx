bb71e493559958f23331dc12c4894e5439dc5125
RicardoRocha27 <rocharicardo2799@gmail.com>
2024-04-20 11:56:51 +0100

    FEAT: add links to footer

--- components/shared/footer/footer-link.tsx
+++ components/shared/footer/footer-link.tsx
@@ -1,15 +1,36 @@
+import { useLocale } from "next-intl";
+
 type FooterLinkProps = {
   title: string;
   links: string[];
 };
 
 export const FooterLink = ({ title, links }: FooterLinkProps) => {
+  const locale = useLocale();
+
+  const MAP_NAMES_TO_LINKS: { [key: string]: string } = {
+    Home: `/${locale}`,
+    Início: `/${locale}`,
+    Services: `/${locale}/services`,
+    Serviços: `/${locale}/services`,
+    Fleet: `/${locale}/fleet`,
+    Frota: `/${locale}/fleet`,
+    About: `/${locale}/about`,
+    Sobre: `/${locale}/about`,
+    Contacts: `/${locale}/contacts`,
+    Contactos: `/${locale}/contacts`,
+  };
+
   return (
     <div>
       <h3 className="mb-2 font-semibold">{title}</h3>
       <div className="flex flex-col space-y-1">
         {links.map((link: string, index: number) => (
-          <a key={index} href="#" className="text-sm hover:underline w-fit">
+          <a
+            key={index}
+            href={MAP_NAMES_TO_LINKS[link]}
+            className="text-sm hover:underline w-fit"
+          >
             {link}
           </a>
         ))}
