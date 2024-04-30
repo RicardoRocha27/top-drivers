13eb5eef6566754afd64f4098a0855d87bd778f9
RicardoRocha27 <rocharicardo2799@gmail.com>
2024-04-30 14:58:05 +0100

    EDIT: responsive car wrapper

--- app/[locale]/fleet/_components/car-wrapper.tsx
+++ app/[locale]/fleet/_components/car-wrapper.tsx
@@ -5,7 +5,7 @@ import { Filter } from "./filter";
 export const CarWrapper = () => {
   return (
     <div className="mx-auto max-w-6xl">
-      <div className="flex items-end justify-between">
+      <div className="flex flex-col md:flex-row md:items-end md:justify-between gap-4">
         <Header translationKeyword="fleet.cars.header" />
         <Filter />
       </div>
