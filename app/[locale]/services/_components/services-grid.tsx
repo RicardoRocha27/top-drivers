8c0e94b36f7dc106bb80ae87641011d57d68bb55
Tiago Pereira <tiagorp2004@gmail.com>
2024-05-23 22:27:33 +0100

    Weird bug

--- app/[locale]/services/_components/services-grid.tsx
+++ app/[locale]/services/_components/services-grid.tsx
@@ -11,18 +11,8 @@ export const ServicesGrid = () => {
         <div className="bg-secondary rounded-lg w-full pt-16 pb-16 lg:pb-32 px-4">
           <div className="max-w-6xl mx-auto">
             <ServiceItem
-<<<<<<< Updated upstream
               firstImageUrl="/home-hero.png"
               secondImageUrl="/home-hero.png"
-              title={t("executive.title")}
-              subtitle={t("executive.subtitle")}
-              firstDescriptionTitle={t("executive.firstDescriptionTitle")}
-              firstDescription={t("executive.firstDescription")}
-              secondDescriptionTitle={t("executive.secondDescriptionTitle")}
-              secondDescription={t("executive.secondDescription")}
-=======
-              firstImageUrl="/home-hero.jpg"
-              secondImageUrl="/home-hero.jpg"
               title={t('executive.title')}
               subtitle={t('executive.subtitle')}
               firstDescriptionTitle={t('executive.firstDescriptionTitle')}
@@ -31,7 +21,6 @@ export const ServicesGrid = () => {
               secondDescription={t('executive.secondDescription')}
               thirdDescriptionTitle={t('executive.thirdDescriptionTitle')}
               thirdDescription={t('executive.thirdDescription')}
->>>>>>> Stashed changes
               id="executive"
               hasExtra
             />
@@ -39,25 +28,14 @@ export const ServicesGrid = () => {
         </div>
         <div className="max-w-6xl mx-auto">
           <ServiceItem
-<<<<<<< Updated upstream
             firstImageUrl="/home-hero.png"
             secondImageUrl="/home-hero.png"
-            title={t("weddings.title")}
-            subtitle={t("weddings.subtitle")}
-            firstDescriptionTitle={t("weddings.firstDescriptionTitle")}
-            firstDescription={t("weddings.firstDescription")}
-            secondDescriptionTitle={t("weddings.secondDescriptionTitle")}
-            secondDescription={t("weddings.secondDescription")}
-=======
-            firstImageUrl="/home-hero.jpg"
-            secondImageUrl="/home-hero.jpg"
             title={t('weddings.title')}
             subtitle={t('weddings.subtitle')}
             firstDescriptionTitle={t('weddings.firstDescriptionTitle')}
             firstDescription={t('weddings.firstDescription')}
             secondDescriptionTitle={t('weddings.secondDescriptionTitle')}
             secondDescription={t('weddings.secondDescription')}
->>>>>>> Stashed changes
             id="weddings"
             isReversed
           />
@@ -65,18 +43,8 @@ export const ServicesGrid = () => {
         <div className="bg-secondary rounded-lg w-full pt-16 pb-32 px-4">
           <div className="max-w-6xl mx-auto">
             <ServiceItem
-<<<<<<< Updated upstream
               firstImageUrl="/home-hero.png"
               secondImageUrl="/home-hero.png"
-              title={t("tours.title")}
-              subtitle={t("tours.subtitle")}
-              firstDescriptionTitle={t("tours.firstDescriptionTitle")}
-              firstDescription={t("tours.firstDescription")}
-              secondDescriptionTitle={t("tours.secondDescriptionTitle")}
-              secondDescription={t("tours.secondDescription")}
-=======
-              firstImageUrl="/home-hero.jpg"
-              secondImageUrl="/home-hero.jpg"
               title={t('tours.title')}
               subtitle={t('tours.subtitle')}
               firstDescriptionTitle={t('tours.firstDescriptionTitle')}
@@ -85,7 +53,6 @@ export const ServicesGrid = () => {
               secondDescription={t('tours.secondDescription')}
               thirdDescriptionTitle={t('tours.thirdDescriptionTitle')}
               thirdDescription={t('tours.thirdDescription')}
->>>>>>> Stashed changes
               id="tours"
               hasExtra
             />
@@ -93,25 +60,14 @@ export const ServicesGrid = () => {
         </div>
         <div className="max-w-6xl mx-auto">
           <ServiceItem
-<<<<<<< Updated upstream
             firstImageUrl="/home-hero.png"
             secondImageUrl="/home-hero.png"
-            title={t("nightLife.title")}
-            subtitle={t("nightLife.subtitle")}
-            firstDescriptionTitle={t("nightLife.firstDescriptionTitle")}
-            firstDescription={t("nightLife.firstDescription")}
-            secondDescriptionTitle={t("nightLife.secondDescriptionTitle")}
-            secondDescription={t("nightLife.secondDescription")}
-=======
-            firstImageUrl="/home-hero.jpg"
-            secondImageUrl="/home-hero.jpg"
             title={t('nightLife.title')}
             subtitle={t('nightLife.subtitle')}
             firstDescriptionTitle={t('nightLife.firstDescriptionTitle')}
             firstDescription={t('nightLife.firstDescription')}
             secondDescriptionTitle={t('nightLife.secondDescriptionTitle')}
             secondDescription={t('nightLife.secondDescription')}
->>>>>>> Stashed changes
             id="night-life"
             isReversed
           />
