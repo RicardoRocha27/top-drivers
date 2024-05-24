9138e88da91cdbbfb1f7f413820d127b7f7b17eb
Tiago Pereira <tiagorp2004@gmail.com>
2024-05-24 17:45:19 +0100

    about banner photo changed

--- app/[locale]/about/_components/banner.tsx
+++ app/[locale]/about/_components/banner.tsx
@@ -1,12 +1,12 @@
-import { Container } from "@/components/shared/container";
-import Image from "next/image";
+import { Container } from '@/components/shared/container';
+import Image from 'next/image';
 
 export const Banner = () => {
   return (
     <Container>
       <div className="w-full my-32 h-[300px] relative">
         <Image
-          src="/about-banner.png"
+          src="/about-banner.jpg"
           alt="banner"
           fill
           className="object-center object-cover rounded-lg"
