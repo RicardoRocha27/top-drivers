e7dc00cd726e28a0ed43db8e44fe5349b513d375
RicardoRocha27 <rocharicardo2799@gmail.com>
2024-05-25 14:51:48 +0100

    EDIT: remove TODO

--- app/[locale]/contacts/_components/faqs.tsx
+++ app/[locale]/contacts/_components/faqs.tsx
@@ -1,30 +1,29 @@
-import { Header } from "@/components/shared/header";
+import { Header } from '@/components/shared/header';
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@/components/ui/accordion";
-import { useTranslations } from "next-intl";
+} from '@/components/ui/accordion';
+import { useTranslations } from 'next-intl';
 
 export const Faqs = () => {
-  const t = useTranslations("contacts.faqs.items");
+  const t = useTranslations('contacts.faqs.items');
   return (
     <div className="my-32 max-w-6xl mx-auto space-y-8">
       <Header translationKeyword="contacts.faqs" />
-      {/* TODO: CHANGE FAQS */}
       <Accordion type="single" collapsible>
         <AccordionItem value="item-1">
-          <AccordionTrigger>{t("question1.question")}</AccordionTrigger>
-          <AccordionContent>{t("question1.answer")}</AccordionContent>
+          <AccordionTrigger>{t('question1.question')}</AccordionTrigger>
+          <AccordionContent>{t('question1.answer')}</AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-2">
-          <AccordionTrigger>{t("question2.question")}</AccordionTrigger>
-          <AccordionContent>{t("question2.answer")}</AccordionContent>
+          <AccordionTrigger>{t('question2.question')}</AccordionTrigger>
+          <AccordionContent>{t('question2.answer')}</AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-3">
-          <AccordionTrigger>{t("question3.question")}</AccordionTrigger>
-          <AccordionContent>{t("question3.answer")}</AccordionContent>
+          <AccordionTrigger>{t('question3.question')}</AccordionTrigger>
+          <AccordionContent>{t('question3.answer')}</AccordionContent>
         </AccordionItem>
       </Accordion>
     </div>
