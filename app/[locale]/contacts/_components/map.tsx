1bac0c66b9ac9ce5c3c02bcbd6f3539401891ad2
RicardoRocha27 <rocharicardo2799@gmail.com>
2024-05-25 22:30:17 +0100

    EDIT: try to fix map loading

--- app/[locale]/contacts/_components/map.tsx
+++ app/[locale]/contacts/_components/map.tsx
@@ -4,12 +4,12 @@ export const Map = () => {
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3110.287224394258!2d-9.136747124068812!3d38.78004937174934!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd19324616d90183%3A0xa66a53e58036d46!2sHumberto%20Delgado%20Airport!5e0!3m2!1sen!2spt!4v1716485360568!5m2!1sen!2spt"
       style={{
         border: 0,
-        borderRadius: "8px",
-        height: "100%",
-        minHeight: "400px",
-        width: "100%",
+        borderRadius: '8px',
+        height: '100%',
+        minHeight: '400px',
+        width: '100%',
       }}
-      loading="lazy"
+      allowFullScreen={false}
     ></iframe>
   );
 };
